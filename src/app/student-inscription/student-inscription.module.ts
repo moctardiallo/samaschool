5de5861a67cd1eed82c0e815615f2eb9ecb1c6c3
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-17 05:03:40 +0000

    Merged branch material-and-flexlayout

--- src/app/student-inscription/student-inscription.module.ts
+++ src/app/student-inscription/student-inscription.module.ts
@@ -1,10 +1,11 @@
+import { MatButtonModule } from "@angular/material/button";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { StudentInscriptionComponent } from "./student-inscription/student-inscription.component";
 
 @NgModule({
   declarations: [StudentInscriptionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [StudentInscriptionComponent]
 })
 export class StudentInscriptionModule {}
