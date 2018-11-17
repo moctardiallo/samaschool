b2b9b696844e857c54fac96de80187f30e1c3404
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-17 06:53:39 +0000

    Added simple information form

--- src/app/student-inscription/student-inscription.module.ts
+++ src/app/student-inscription/student-inscription.module.ts
@@ -1,11 +1,24 @@
-import { MatButtonModule } from "@angular/material/button";
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatStepperModule
+} from "@angular/material";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { StudentInscriptionComponent } from "./student-inscription/student-inscription.component";
 
 @NgModule({
   declarations: [StudentInscriptionComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatStepperModule
+  ],
   exports: [StudentInscriptionComponent]
 })
 export class StudentInscriptionModule {}
--- src/app/student-inscription/student-inscription/student-inscription.component.ts
+++ src/app/student-inscription/student-inscription/student-inscription.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'student-inscription',
-  templateUrl: './student-inscription.component.html',
-  styleUrls: ['./student-inscription.component.css']
+  selector: "student-inscription",
+  templateUrl: "./student-inscription.component.html",
+  styleUrls: ["./student-inscription.component.css"]
 })
 export class StudentInscriptionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
